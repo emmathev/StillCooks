c8a6786a422288eccc389442cf8fde4fcc9c0d13
Ebenezer Mathevani <emmathevani@gmail.com>
2024-11-13 11:38:01 +0200

    repo created and added js and css

---
+++ src/main/java/za/ac/tut/StillCooks/StillCooksApplication.java
@@ -0,0 +1,13 @@
+package za.ac.tut.StillCooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StillCooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StillCooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/ac/tut/StillCooks/StillCooksApplicationTests.java
@@ -0,0 +1,13 @@
+package za.ac.tut.StillCooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StillCooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
